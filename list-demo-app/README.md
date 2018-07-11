1f9e6830bf7f18fc11177307cb66cce69a07e25b
kim-marcel <kim.feichtinger@gmx.de>
2018-07-11 22:02:47 +0200

    refactor name of app
    - refactor name of app from "list-demo-api" to "list-demo-app"

--- list-demo-app/README.md
+++ list-demo-app/README.md
@@ -1,4 +1,4 @@
-# ListDemoApi
+# ListDemoApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
--- list-demo-app/e2e/src/app.e2e-spec.ts
+++ list-demo-app/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to list-demo-api!');
+    expect(page.getParagraphText()).toEqual('Welcome to list-demo-app!');
   });
 });
