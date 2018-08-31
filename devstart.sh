2b00ed3906cf8e84943598e896aae145867fefbf
kim-marcel <kim.feichtinger@gmx.de>
2018-08-31 20:27:49 +0200

    change pm2-process name
    - names for the processes was previously set to list-info-*, which was a typo

--- devstart.sh
+++ devstart.sh
@@ -6,9 +6,9 @@ pm2 delete all
 
 # start api
 cd list-demo-api
-pm2 start dev_appserver.py --name "list-info-api" -- --clear_datastore=yes --dev_appserver_log_level=debug app.yaml
+pm2 start dev_appserver.py --name "list-demo-api" -- --clear_datastore=yes --dev_appserver_log_level=debug app.yaml
 cd ..
 
 # start app
 cd list-demo-app
-pm2 start ng --name "list-info-app" -- serve
+pm2 start ng --name "list-demo-app" -- serve
