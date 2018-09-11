5f8841c10d17cac3fe126e3353b13809d2ef1138
kim-marcel <kim.feichtinger@gmx.de>
2018-09-12 00:28:32 +0200

    refactor modules
    - no subfolders for each module in the modules folder anymore

--- packages/list-demo-app/src/app/app.module.ts
+++ packages/list-demo-app/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AuthService, ListResolver, ListService, TextService } from './services'
 import { environment } from '../environments/environment';
 import { ErrorPageComponent, HomePageComponent, ListPageComponent, LoginPageComponent } from './pages';
 import { HeaderComponent, ListComponent, LoginComponent, LogoutComponent } from './components';
-import { RoutingModule } from './modules/routing/routing.module';
+import { RoutingModule } from './modules/routing.module';
 import { TextPipe } from './pipes/text.pipe';
 
 @NgModule({
