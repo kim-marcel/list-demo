768b0641da8248b180bee38e0557a60d15447096
kim-marcel <kim.feichtinger@gmx.de>
2018-09-15 16:50:11 +0200

    add disclaimer

--- packages/list-demo-app/src/app/resources/texts.ts
+++ packages/list-demo-app/src/app/resources/texts.ts
@@ -5,12 +5,14 @@ export const texts = {
   'app.component.header.button.logout': 'Logout',
 
   'app.component.list.button.add': 'Add',
-  'app.component.list.input.placeholder': 'Input',
+  'app.component.list.input.placeholder': 'Type something here you want to add to your list...',
 
   'app.component.login.button.google-login': 'Login with Google',
   'app.component.logout.button.logout': 'Logout',
 
   // pages
+  'app.page.list.alert.disclaimer': 'Disclaimer: This Website is just a demo. Please do not use this website to store any important information. Your data is not secured and can be deleted at any time. This website can go offline temporarily or permanently.',
+
   'app.page.home.title': 'Demo Shopping List App',
   'app.page.home.text.lead': 'This website allows you to have your shopping-list always with you. Wherever you are. Whenever you need it. Online.',
   'app.page.home.text.login': 'Please login to view your list:',
